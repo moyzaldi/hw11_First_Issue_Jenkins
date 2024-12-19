ae5147610b9f2deeb7f0a381af22058549a3a824
Tatiana Konischeva <moyzaldi@gmail.com>
2024-12-19 22:28:20 +0300

    added demoqa test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,31 @@
 import allure
-from selene import browser
 import pytest
+from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture()
 def browser_settings():
     with allure.step("Параметры браузера"):
-        driver_options = webdriver.ChromeOptions()
-        driver_options.page_load_strategy = 'eager'
-        browser.config.driver_options = driver_options
-        browser.config.window_height = 1080
-        browser.config.window_width = 1920
-        browser.config.base_url = 'https://demoqa.com'
-        browser.config.timeout = 20
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": "126.0",
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            }
+        }
 
-    yield
-    browser.quit()
+        options.capabilities.update(selenoid_capabilities)
+        driver = webdriver.Remote(
+            command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+            options=options)
+
+        browser.config.driver = driver
+
+        browser.config.base_url = 'https://demoqa.com'
+
+        yield
+        browser.quit()
