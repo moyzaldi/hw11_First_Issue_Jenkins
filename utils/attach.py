b1b3ee33f114bba2c3cc283c2f73aeaf196cee71
Tatiana Konischeva <moyzaldi@gmail.com>
2024-12-20 00:09:58 +0300

    Исправлен video_url

--- utils/attach.py
+++ utils/attach.py
@@ -14,6 +14,7 @@ def add_logs(browser):
     allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
 
 
+
 # html-код страницы
 def add_html(browser):
     html = browser.driver.page_source
@@ -22,7 +23,7 @@ def add_html(browser):
 
 # скринкаст
 def add_video(browser):
-    video_url = f"{selenoid_url}/video/" + browser.driver.session_id + ".mp4"
+    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
            + "' type='video/mp4'></video></body></html>"
